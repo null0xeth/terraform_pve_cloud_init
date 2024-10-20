17f289d25e3d2b81affe74b3cc8a95c05b8e9120
null0xeth <112922737+null0xeth@users.noreply.github.com>
2024-10-20 14:17:03 +0200

    .

--- main.tf
+++ main.tf
@@ -20,19 +20,6 @@ locals {
   }
 }
 
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.64.0"
-    }
-    proxmox = {
-      source  = "bpg/proxmox"
-      version = ">=0.62.0"
-    }
-  }
-}
-
 
 resource "proxmox_virtual_environment_file" "cloud_config" {
   content_type = "snippets"
---
+++ versions.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.64.0"
+    }
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = ">=0.62.0"
+    }
+  }
+}
